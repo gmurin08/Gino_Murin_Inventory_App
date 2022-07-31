12043edc822d7b6b42812d74cd14a2488edad168
Gino Murin <gmurin@gmail.com>
2022-07-30 22:54:26 -0400

    created user class

--- app/src/main/java/com/zybooks/gino_murin_inventory_app/User.java
+++ app/src/main/java/com/zybooks/gino_murin_inventory_app/User.java
@@ -2,4 +2,29 @@ package com.zybooks.gino_murin_inventory_app;
 
 public class User {
 
+    private String username;
+    private String password;
+
+    public User(){};
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setUsername(){
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
